10acd49d09170c971122612fc0fb534bbe3dc43b
Nikuruuu <92920102+Nikuruuu@users.noreply.github.com>
2024-04-05 15:59:14 +0800

    crud endpoints and tests

--- routers/users.py
+++ routers/users.py
@@ -6,10 +6,17 @@ from utils import password_utils
 from db.database import get_db
 from utils.file_utils import save_image
 from pydantic import ValidationError
-from typing import Optional
+from typing import List, Optional
 
 
 router = APIRouter(prefix="/users", tags=["Users"])
+
+
+# GET All Users
+@router.get("/", response_model=List[schemas.UserOut])
+def get_all_users(db: Session = Depends(get_db)):
+    users = db.query(model.User).all()
+    return users
 
 
 # GET User
@@ -65,30 +72,41 @@ async def create_user(
 
 
 # PUT User
-@router.put("/{id}")
+@router.put("/{id}", response_model=schemas.UserOut)
 async def update_user(
     id: str,
-    post_user: str = Body(None),
+    post_user: Optional[str] = Body(None),
     profile_image: Optional[UploadFile] = None,
     db: Session = Depends(get_db),
 ):
     # Retrieve the user
     user = db.query(model.User).filter(model.User.id == id).first()
-    # Convert the post_user string to a dictionary
-    post_user = schemas.UserUpdate.parse_raw(post_user)
 
     # If the user doesn't exist, return an error
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
 
-    # Update the user's data
-    user_data = post_user.dict(exclude_unset=True)
-    if post_user.password:
-        hashed_password = password_utils.hash(post_user.password)
-        user_data["password"] = hashed_password
+    # Convert the post_user string to a dictionary if it's not None
+    if post_user is not None:
+        post_user = schemas.UserUpdate.parse_raw(post_user)
+        user_data = post_user.dict(exclude_unset=True)
+        if post_user.password:
+            hashed_password = password_utils.hash(post_user.password)
+            user_data["password"] = hashed_password
+    else:
+        user_data = {}
 
     # Handle the profile_image field
     if profile_image:
+        # Check if the file is a .jpg or .png file
+        if not (
+            profile_image.filename.endswith(".jpg")
+            or profile_image.filename.endswith(".png")
+        ):
+            raise HTTPException(
+                status_code=400,
+                detail="Invalid file type. Only .jpg and .png files are accepted",
+            )
         # Save the uploaded image to a file and store the file path in the database
         image_path = await save_image(profile_image)
         user_data["profile_image"] = image_path
@@ -97,11 +115,12 @@ async def update_user(
         setattr(user, key, value)
 
     db.commit()
+    db.refresh(user)
     return user
 
 
 # Delete User
-@router.delete("/{id}")
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(id: str, db: Session = Depends(get_db)):
     # Retrieve the user
     user = db.query(model.User).filter(model.User.id == id).first()
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,3 +1,37 @@
+import uuid
+import json
+
+
+# GET test
+def test_get_user(client, test_user):
+    # Test retrieving a user
+    response = client.get(f"/users/{test_user['id']}")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["email"] == test_user["email"]
+    assert data["first_name"] == test_user["first_name"]
+    assert data["last_name"] == test_user["last_name"]
+    assert "password" not in data
+
+
+# GET all users test
+def test_get_all_users(client):
+    # Test retrieving all users
+    response = client.get("/users")
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data, list)  # The response should be a list of users
+
+
+# GET user not found test
+def test_user_not_found(client):
+    # Test retrieving a user that does not exist
+    non_existent_id = uuid.uuid4()  # Generate a random UUID
+    response = client.get(f"/users/{non_existent_id}")
+    assert response.status_code == 404  # The server should return a 404 status code
+
+
+# POST test
 def test_create_user(client):
     # Test creating a new user
     with open("images/frieren.jpg", "rb") as img_file:
@@ -53,3 +87,85 @@ def test_create_user_with_invalid_image_type(client):
     assert response.status_code == 400
     data = response.json()
     assert data["detail"] == "Invalid file type. Only .jpg and .png files are accepted"
+
+
+# PUT test
+def test_update_user(client, test_user):
+    # Test updating a user
+    update_data = {
+        "first_name": "Update Name",
+        "last_name": "Update Surname",
+    }
+    files = {
+        "post_user": (None, json.dumps(update_data), "application/json"),
+    }
+    response = client.put(
+        f"/users/{str(test_user['id'])}",
+        files=files,
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert data["first_name"] == update_data["first_name"]
+    assert data["last_name"] == update_data["last_name"]
+
+
+def test_update_user_image_only(client, test_user):
+    # Test updating a user's image
+    with open("images/gon.jpg", "rb") as image_file:
+        files = {
+            "profile_image": image_file,
+        }
+        response = client.put(
+            f"/users/{str(test_user['id'])}",
+            files=files,
+        )
+    assert response.status_code == 200
+
+
+def test_update_user_unsupported_file_type(client, test_user):
+    # Test updating a user's image with an unsupported file type
+    with open("images/unsupported_file_type.txt", "rb") as image_file:
+        files = {
+            "profile_image": image_file,
+        }
+        response = client.put(
+            f"/users/{str(test_user['id'])}",
+            files=files,
+        )
+    assert response.status_code == 400
+
+
+def test_update_user_and_image(client, test_user):
+    # Test updating a user's data and image
+    updated_user_data = {
+        "email": "newemail@example.com",
+        "password": "newpassword",
+    }
+    with open("images/gon.jpg", "rb") as image_file:
+        files = {
+            "post_user": (None, json.dumps(updated_user_data), "application/json"),
+            "profile_image": image_file,
+        }
+        response = client.put(
+            f"/users/{str(test_user['id'])}",
+            files=files,
+        )
+    assert response.status_code == 200
+    updated_user = response.json()
+    assert updated_user["email"] == "newemail@example.com"
+    # Check that the profile_image field has been updated
+    assert updated_user["profile_image"] is not None
+
+
+# Delete Test
+def test_delete_user(client, test_user):
+    response = client.delete(
+        f"/users/{str(test_user['id'])}",
+    )
+    assert response.status_code == 204
+
+
+def test_delete_user_non_exist(client):
+    non_existent_uuid = uuid.uuid4()
+    res = client.delete(f"/users/{non_existent_uuid}")
+    assert res.status_code == 404
